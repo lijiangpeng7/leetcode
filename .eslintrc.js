99a97a7767bb9e78354c29d16c9efb67e5fcde1e
起风了 <970905469@qq.com>
2021-04-11 16:04:52 +0800

    ✨feat: 单元测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "vue/no-unused-components": "off",
     'no-mixed-spaces-and-tabs': 0
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
